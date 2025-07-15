b20a00a3d6f25969758be87da3efc80cc95da470
Mayuka Channankaiah <Mayuka.C@ibm.com>
2025-07-15 20:28:13 +0530

    Fix FAQ links in Login Page, Navbar and QuickLinks
    Minor fix
    
    User guide update
    
    Comments

--- src/components/PopUp/NewUserGuide.jsx
+++ src/components/PopUp/NewUserGuide.jsx
@@ -13,7 +13,7 @@ const NewUserGuide = ({ setActionProps }) => {
         setActionProps("");
       }}
       onRequestSubmit={() => {
-        alert('hi')
+        setActionProps("");
       }}
       open={true}
       primaryButtonText={"Ok"}
@@ -24,15 +24,19 @@ const NewUserGuide = ({ setActionProps }) => {
           <h6>Step: 1</h6>
           Please add your public key in "My Keys" Section which will be used to login into the Power instance.
           <br />
+          <br />
           <h6>Step 2</h6>
           Once group access is approved, you can directly go to the "Catalog" section and choose one of the enabled catalogs as per your requirement. Click deploy.
            <br />
+           <br />
           <h6>Step 3: </h6>
           Enter details for your service (Power instance) and submit. Within few secs, instance will be available to access under "My Services" section.
+          <br />
+          <br />
+          For more information, please read our <a href="https://github.com/PDeXchange/pac-support/blob/main/docs/FAQ.md" target="_blank" rel="noopener noreferrer">FAQ</a> page.
       </div>
       </div>
     </Modal>
-    
   );
 }
 export default NewUserGuide;
